2ac6f2b490a5f2c667351a0747097868066110ac
IamMe <forestsound520@gmail.com>
2026-01-14 03:39:44 +0800

    修改部署base名稱

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/React-vite-week2/',
+  base: '/React-vite-week3/',
   plugins: [react()],
 })
